137975127c00a6909f5460cd295270e29c79e860
Charles Moulliard <cmoulliard@ibm.com>
2026-01-07 17:34:35 +0100

    Add new sample using a rewrite YAML file and use the released version: 0.0.30
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

---
+++ sample/rewritescanner-recipes-yaml/src/main/java/dev/snowdrop/RewriteApp.java
@@ -0,0 +1,43 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS dev.snowdrop.openrewrite:rewrite-standalone-cli:0.0.30
+//DEPS org.junit.jupiter:junit-jupiter-api:5.13.4
+//NOINTEGRATIONS
+
+package dev.snowdrop;
+
+import dev.snowdrop.openrewrite.cli.RewriteScanner;
+import dev.snowdrop.openrewrite.cli.model.Config;
+import dev.snowdrop.openrewrite.cli.model.ResultsContainer;
+import org.junit.jupiter.api.Assertions;
+import org.openrewrite.DataTable;
+import org.openrewrite.RecipeRun;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class RewriteApp {
+    public static void main(String[] args) {
+        String RECIPE_NAME = "dev.snowdrop.FormatCode";
+
+        Config cfg = new Config();
+        cfg.setAppPath(Paths.get("/PATH/TO/rewrite-standalone-cli/test-project/simple"));
+        cfg.setYamlRecipes("rewrite.yml");
+
+        RewriteScanner scanner = new RewriteScanner(cfg);
+        scanner.init();
+        try {
+            ResultsContainer results = scanner.run();
+            RecipeRun run = results.getRecipeRuns().get(RECIPE_NAME);
+
+            Optional<Map.Entry<DataTable<?>, List<?>>> resultMap = run.getDataTables().entrySet().stream()
+                .filter(entry -> entry.getKey().getName().contains("SearchResults"))
+                .findFirst();
+            Assertions.assertFalse(resultMap.isPresent());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
