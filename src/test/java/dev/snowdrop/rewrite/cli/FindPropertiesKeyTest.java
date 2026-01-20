0c6294e6f2123dfd3c0ddde4aa651044a1e4e2c5
Charles Moulliard <cmoulliard@ibm.com>
2026-01-20 17:42:18 +0100

    Created a test case to verify the search recipe: org.openrewrite.properties.search.FindProperties
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

---
+++ src/test/java/dev/snowdrop/rewrite/cli/FindPropertiesKeyTest.java
@@ -0,0 +1,90 @@
+package dev.snowdrop.rewrite.cli;
+
+import dev.snowdrop.openrewrite.cli.RewriteCommand;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DataTable;
+import org.openrewrite.RecipeRun;
+import org.openrewrite.table.SearchResults;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+public class FindPropertiesKeyTest extends BaseTest {
+
+    @Test
+    void shouldFindJpaPropertiesKeyExactly() throws Exception {
+        RewriteCommand rewriteCmd = new RewriteCommand();
+
+        String appPath = "test-project/demo-spring-boot-todo-app";
+        Path rewritePatchFile = Paths.get(appPath, "target/rewrite/rewrite.patch");
+        String recipeName = "org.openrewrite.properties.search.FindProperties";
+
+        // Configure the application to scan and recipe to be executed
+        cfg.setAppPath(Paths.get(appPath));
+        cfg.setFqNameRecipe(recipeName);
+        cfg.setRecipeOptions(Set.of("propertyKey=spring.jpa.hibernate.ddl-auto","relaxedBinding=false"));
+
+        var results = rewriteCmd.execute(cfg);
+        RecipeRun run = results.getRecipeRuns().get(recipeName);
+
+        String patchContent = Files.readString(rewritePatchFile);
+        assertNotNull(patchContent);
+
+        assertFalse(run.getDataTables().isEmpty());
+        Optional<Map.Entry<DataTable<?>, List<?>>> resultMap = run.getDataTables().entrySet().stream()
+            .filter(entry -> entry.getKey().getName().contains("SearchResults"))
+            .findFirst();
+        assertTrue(resultMap.isPresent());
+
+        List<?> rows = resultMap.get().getValue();
+        assertEquals(1, rows.size());
+
+        SearchResults.Row record = (SearchResults.Row)rows.getFirst();
+        assertEquals("src/main/resources/application.properties",record.getSourcePath());
+        assertEquals("spring.jpa.hibernate.ddl-auto=update",record.getResult());
+        assertEquals("Find property `spring.jpa.hibernate.ddl-auto`",record.getRecipe());
+    }
+
+    @Test
+    void shouldFindJpaPropertiesKeyRelaxedBinding() throws Exception {
+        RewriteCommand rewriteCmd = new RewriteCommand();
+
+        String appPath = "test-project/demo-spring-boot-todo-app";
+        Path rewritePatchFile = Paths.get(appPath, "target/rewrite/rewrite.patch");
+        String recipeName = "org.openrewrite.properties.search.FindProperties";
+
+        // Configure the application to scan and recipe to be executed
+        cfg.setAppPath(Paths.get(appPath));
+        cfg.setFqNameRecipe(recipeName);
+        cfg.setRecipeOptions(Set.of("propertyKey=spring.jpa.hibernate.ddlAuto","relaxedBinding=true"));
+
+        var results = rewriteCmd.execute(cfg);
+        RecipeRun run = results.getRecipeRuns().get(recipeName);
+
+        String patchContent = Files.readString(rewritePatchFile);
+        assertNotNull(patchContent);
+
+        assertFalse(run.getDataTables().isEmpty());
+        Optional<Map.Entry<DataTable<?>, List<?>>> resultMap = run.getDataTables().entrySet().stream()
+            .filter(entry -> entry.getKey().getName().contains("SearchResults"))
+            .findFirst();
+        assertTrue(resultMap.isPresent());
+
+        List<?> rows = resultMap.get().getValue();
+        assertEquals(1, rows.size());
+
+        SearchResults.Row record = (SearchResults.Row)rows.getFirst();
+        assertEquals("src/main/resources/application.properties",record.getSourcePath());
+        assertEquals("spring.jpa.hibernate.ddl-auto=update",record.getResult());
+        assertEquals("Find property `spring.jpa.hibernate.ddlAuto`",record.getRecipe());
+    }
+}
