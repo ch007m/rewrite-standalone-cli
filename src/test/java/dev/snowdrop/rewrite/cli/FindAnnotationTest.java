a4023d7f0151fa631b96e523e7eced08ef37818d
Charles Moulliard <cmoulliard@ibm.com>
2026-01-21 11:02:50 +0100

    Add a @BeforeEach method to clean up the target/rewrite folder between the tests
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- src/test/java/dev/snowdrop/rewrite/cli/FindAnnotationTest.java
+++ src/test/java/dev/snowdrop/rewrite/cli/FindAnnotationTest.java
@@ -2,42 +2,49 @@ package dev.snowdrop.rewrite.cli;
 
 import dev.snowdrop.openrewrite.cli.RewriteCommand;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DataTable;
 import org.openrewrite.RecipeRun;
 import org.openrewrite.table.SearchResults;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 public class FindAnnotationTest extends BaseTest {
 
+    String appPath = "test-project/demo-spring-boot-todo-app";
+
+    @BeforeEach
+    public void setup() throws IOException {
+        Path pathToBeDeleted = Paths.get(appPath, "target/rewrite");
+        if (Files.exists(pathToBeDeleted)) {
+            try (Stream<Path> paths = Files.walk(pathToBeDeleted)) {
+                paths.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            }
+        }
+    }
+
     @Test
     void shouldFindAnnotation() throws Exception {
         RewriteCommand rewriteCmd = new RewriteCommand();
-
-        String appPath = "test-project/demo-spring-boot-todo-app";
-        Path rewritePatchFile = Paths.get(appPath, "target/rewrite/rewrite.patch");
         String recipeName = "org.openrewrite.java.search.FindAnnotations";
 
         // Configure the application to scan and recipe to be executed
         cfg.setAppPath(Paths.get(appPath));
         cfg.setFqNameRecipe(recipeName);
-        cfg.setRecipeOptions(Set.of("annotationPattern=@org.springframework.boot.autoconfigure.SpringBootApplication,matchMetaAnnotations=false"));
+        cfg.setRecipeOptions(Set.of("annotationPattern=org.springframework.boot.autoconfigure.SpringBootApplication","matchMetaAnnotations=false"));
 
         var results = rewriteCmd.execute(cfg);
         RecipeRun run = results.getRecipeRuns().get(recipeName);
-
-        String patchContent = Files.readString(rewritePatchFile);
-        assertNotNull(patchContent);
 
         assertFalse(run.getDataTables().isEmpty());
         Optional<Map.Entry<DataTable<?>, List<?>>> resultMap = run.getDataTables().entrySet().stream()
@@ -48,9 +55,9 @@ public class FindAnnotationTest extends BaseTest {
         List<?> rows = resultMap.get().getValue();
         assertEquals(1, rows.size());
 
-        SearchResults.Row record = (SearchResults.Row)rows.getFirst();
-        assertEquals("src/main/java/com/todo/app/AppApplication.java",record.getSourcePath());
-        assertEquals("@SpringBootApplication",record.getResult());
-        assertEquals("Find annotations `@org.springframework.boot.autoconfigure.SpringBootApplication,matchMetaAnnotations=false`",record.getRecipe());
+        SearchResults.Row record = (SearchResults.Row) rows.getFirst();
+        assertEquals("src/main/java/com/todo/app/AppApplication.java", record.getSourcePath());
+        assertEquals("@SpringBootApplication", record.getResult());
+        assertEquals("Find annotations `org.springframework.boot.autoconfigure.SpringBootApplication`", record.getRecipe());
     }
 }
--- src/test/java/dev/snowdrop/rewrite/cli/FindPropertiesKeyTest.java
+++ src/test/java/dev/snowdrop/rewrite/cli/FindPropertiesKeyTest.java
@@ -2,29 +2,41 @@ package dev.snowdrop.rewrite.cli;
 
 import dev.snowdrop.openrewrite.cli.RewriteCommand;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DataTable;
 import org.openrewrite.RecipeRun;
 import org.openrewrite.table.SearchResults;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 public class FindPropertiesKeyTest extends BaseTest {
 
+    String appPath = "test-project/demo-spring-boot-todo-app";
+
+    @BeforeEach
+    public void setup() throws IOException {
+        Path pathToBeDeleted = Paths.get(appPath, "target/rewrite");
+        if (Files.exists(pathToBeDeleted)) {
+            try (Stream<Path> paths = Files.walk(pathToBeDeleted)) {
+                paths.sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            }
+        }
+    }
+
     @Test
     void shouldFindJpaPropertiesKeyExactly() throws Exception {
         RewriteCommand rewriteCmd = new RewriteCommand();
 
-        String appPath = "test-project/demo-spring-boot-todo-app";
         Path rewritePatchFile = Paths.get(appPath, "target/rewrite/rewrite.patch");
         String recipeName = "org.openrewrite.properties.search.FindProperties";
 
